44d373b6347ca354ef2fa3b969d5a679eaaca9ac
Harrison Hoggard <harrisonhoggard@gmail.com>
2024-01-26 19:41:33 -0600

    Reworded logging

--- src/main/java/events/DefaultChannelDelete.java
+++ src/main/java/events/DefaultChannelDelete.java
@@ -6,6 +6,8 @@ import events.util.EventObject;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.channel.ChannelDeleteEvent;
+import net.dv8tion.jda.api.exceptions.ErrorResponseException;
+import net.dv8tion.jda.api.exceptions.InsufficientPermissionException;
 
 import java.util.Objects;
 
@@ -30,11 +32,17 @@ public class DefaultChannelDelete extends EventObject {
         if (event.getChannel().getId().equals(Bot.defaultChannels.get(event.getGuild()).getId()))
         {
             Guild guild = event.getGuild();
-            Objects.requireNonNull(guild.getOwner()).getUser().openPrivateChannel().flatMap(channel -> channel.sendMessageEmbeds(new EmbedBuilder()
-                    .addField("Default Channel deleted", "Hello, the text channel I was using for your server named \"" + guild.getName() + "\" was recently deleted." +
-                            "\nIn order for me to function properly, I will need a default channel to send messages in, and you can set that up by typing in **\"" + Config.get("COMMAND_PREFIX") +
-                            " channel\"** inside the text channel you want me to use.\n\nThank you", false).build()))
-                    .queue();
+            try {
+                Objects.requireNonNull(guild.getOwner()).getUser().openPrivateChannel().flatMap(channel -> channel.sendMessageEmbeds(new EmbedBuilder()
+                        .addField("Default Channel deleted", "Hello, the text channel I was using for your server named \"" + guild.getName() + "\" was recently deleted." +
+                                "\nIn order for me to function properly, I will need a default channel to send messages in, and you can set that up by typing in **\"" + Config.get("COMMAND_PREFIX") +
+                                " channel\"** inside the text channel you want me to use.\n\nThank you", false).build()))
+                        .queue();
+            } catch (InsufficientPermissionException | ErrorResponseException e) {
+                Bot.log(getLogType(), "Could not private message " + Objects.requireNonNull(guild.getOwner()).getId() + " from guild " + guild.getId() + " because of a deleted default channel");
+                Bot.defaultChannels.put(guild, null);
+                return;
+            }
 
             devMessage(getName(), getAction(), guild);
         }
